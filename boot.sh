f578ad704b7d750c6883198c11daa009124246e2
Oliver Richardson <oliver@bow-data.com>
2022-11-02 16:56:38 +0000

    Azure Docker stage"

--- boot.sh
+++ boot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source venv/bin/activate
-exec gunicorn -b :5000 --access-logfile - --error-logfile - bowdata.test.flask:create_app()
+exec gunicorn -b localhost:5000 --access-logfile - --error-logfile - bowdata.test.flask:create_app\(\)
